4e5f106c60af33bb0edcd3769496a136fa9ea099
KafilAbbas <141447943+KafilAbbas@users.noreply.github.com>
2023-08-25 01:34:39 +0530

    Delete api directory

--- api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id','stuname','email']
-# Register your models here.
-admin.site.register(DataFrameStorage)
